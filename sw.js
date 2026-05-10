5b3bb7bc7e98244e9d265f7cf21b8a63b2abd9f3
benallen-cmyk <benjaminallen846@gmail.com>
2026-05-10 13:17:42 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'trailhead-basecamp-v1';
+const CACHE = 'trailhead-basecamp-v2';
 const ASSETS = [
   '/trailhead-basecamp/',
   '/trailhead-basecamp/index.html',
+  '/trailhead-basecamp/manifest.json',
+  '/trailhead-basecamp/icon-192.png',
+  '/trailhead-basecamp/icon-512.png',
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
 });
 
@@ -21,11 +25,11 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (!res || res.status !== 200 || res.type !== 'basic') return res;
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
+      return cached || fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
         return res;
       }).catch(() => caches.match('/trailhead-basecamp/index.html'));
     })
